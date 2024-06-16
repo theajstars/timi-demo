950b8fd423fe5c83340cd47212f1b7b66f7a550a
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2024-06-16 02:30:49 +0100

    :tada:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, createTheme, MantineProvider, Checkbox } from "@mantine/core";
 
+import "@mantine/core/styles.css";
+import { Home } from "./Pages/Home";
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={theme}>
+      <Home />
+      <Button>Click me</Button>
+      <Checkbox defaultChecked label="I agree to sell my privacy" />
+    </MantineProvider>
   );
 }
 

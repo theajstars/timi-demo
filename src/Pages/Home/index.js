2496c7fa4dd2d118384cca4a61588808939d3d61
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2024-06-19 22:55:35 +0100

    :construction:

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,30 +1,49 @@
 import { useState } from "react";
+
 import "./styles.css";
+
 export default function Home() {
   const [activeTab, setActiveTab] = useState("one");
   return (
     <div className="container">
       <div className="tabs">
-        <span
+        <button
+          className={`tab ${activeTab === "one" ? "tab-active" : "tab-inactive"}`}
           onClick={() => {
             setActiveTab("one");
           }}
-          className={`tab ${
-            activeTab === "one" ? "tab-active" : "tab-inactive"
-          }`}
         >
-          Tab One
-        </span>
-        <span
+          Information Systems
+        </button>
+        <button
+          className={`tab ${activeTab === "two" ? "tab-active" : "tab-inactive"}`}
           onClick={() => {
             setActiveTab("two");
           }}
-          className={`tab ${
-            activeTab === "two" ? "tab-active" : "tab-inactive"
-          }`}
         >
-          Tab Two
-        </span>
+          Electrical Parts
+        </button>
+      </div>
+      <div className="content">
+        {activeTab === "one" ? (
+          <>
+            <h1>Information Systems Content</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate rem quibusdam odio veritatis
+            modi hic quis fuga quisquam? Dignissimos eligendi nostrum assumenda perspiciatis impedit alias
+            odit sed optio. Distinctio, dignissimos! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptate rem quibusdam odio veritatis modi hic quis fuga quisquam? Dignissimos eligendi nostrum
+            assumenda perspiciatis impedit alias odit sed optio. Distinctio, dignissimos!
+          </>
+        ) : (
+          <>
+            <h1>Electrical Parts Content</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate rem quibusdam odio veritatis
+            modi hic quis fuga quisquam? Dignissimos eligendi nostrum assumenda perspiciatis impedit alias
+            odit sed optio. Distinctio, dignissimos! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptate rem quibusdam odio veritatis modi hic quis fuga quisquam? Dignissimos eligendi nostrum
+            assumenda perspiciatis impedit alias odit sed optio. Distinctio, dignissimos!
+          </>
+        )}
       </div>
     </div>
   );

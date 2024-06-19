d5ccd177ae05294df39c105506d68eaacd825f9d
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2024-06-19 22:16:55 +0100

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Button, createTheme, MantineProvider, Checkbox } from "@mantine/core";
 
 import "@mantine/core/styles.css";
-import { Home } from "./Pages/Home";
+import Home from "./Pages/Home";
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
@@ -9,8 +9,6 @@ function App() {
   return (
     <MantineProvider theme={theme}>
       <Home />
-      <Button>Click me</Button>
-      <Checkbox defaultChecked label="I agree to sell my privacy" />
     </MantineProvider>
   );
 }
